3eca674b119a306309a8422ba20ba0bfa1865d52
Annie <ikram.annie0@gmail.com>
2021-05-16 16:18:05 -0400

    downloaded sinon, sinon-chai, created mocks and tests/controller files

---
+++ tests/controller/mocks/villains.js
@@ -0,0 +1,18 @@
+const villainsList = [{
+  name: 'Shan Yu',
+  movie: 'Mulan',
+  slug: 'shan-yu'
+},
+{
+  name: 'Shere Khan',
+  movie: 'The Jungle Book',
+  slug: 'shere-khan'
+}]
+
+const singleVillain = {
+  name: 'Red Skull',
+  movie: 'Captain America: The First Avenger',
+  slug: 'red-skull'
+}
+
+module.exports = { villainsList, singleVillain }
